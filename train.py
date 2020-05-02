a0ddcfe554b8c8bacaf9892529c16cacb45647f8
Vikram Raj <dr.vikramraj87@live.com>
2020-05-02 23:20:14 +0530

    vision/datasets/voc.py VOC dataset created

---
+++ train.py
@@ -0,0 +1,8 @@
+from vision.datasets.voc import VOC
+from pathlib import Path
+
+def train():
+    train_dataset = Path("./data/VOCdevkit/VOC2007").resolve()
+
+if __name__ == '__main__':
+    train()
--- vision/datasets/voc.py
+++ vision/datasets/voc.py
@@ -1,6 +1,9 @@
 from torch.utils.data import Dataset
 from pathlib import Path
 import xml.etree.ElementTree as ET
+import numpy as np
+from PIL import Image
+from vision.image_bbox import ImageBbox
 
 
 class VOC(Dataset):
@@ -30,8 +33,11 @@ class VOC(Dataset):
     def __len__(self):
         return len(self.ids)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> ImageBbox:
         id_ = self.ids[item]
+        bbox, label, difficult = self._annotations(id_)
+        img = self._image(id_)
+        return ImageBbox(img, bbox, label, difficult)
 
     def _annotations(self, id_: str):
         xml = self.annot_dir / f"{id_}.xml"
@@ -45,10 +51,39 @@ class VOC(Dataset):
             diff = int(obj.find("difficult").text)
             if not self.use_difficult and diff == 1:
                 continue
-
             difficult.append(diff)
+
             bbox_xml = obj.find("bndbox")
-            bbox = []
+            box = [int(bbox_xml.find(tag).text) - 1
+                   for tag in ('ymin', 'xmin', 'ymax', 'xmax')]
+            bbox.append(box)
+
+            name = obj.find("name").text.lower().strip()
+            lbl = VOC.label_names.index(name)
+            label.append(lbl)
+
+        bbox = np.stack(bbox).astype(np.float32)
+        # TODO: uint8?
+        label = np.array(label).astype(np.int32)
+        # TODO: Check support for np.bool in pytorch
+        difficult = np.array(difficult, dtype=np.bool).astype(np.uint8)
+
+        return bbox, label, difficult
+
+    def _image(self, id_: str, dtype=np.float32) -> np.ndarray:
+        file = self.image_dir / f"{id_}.jpg"
+        img = None
+
+        with Image.open(file) as f:
+            img = f.convert("RGB")
+            img = np.asarray(img, dtype=dtype)
+
+        if img.ndim == 2:
+            # (H, W) -> (1, H, W)
+            return img[np.newaxis, :]
+
+        # (H, W, C) -> (C, H, W)
+        return img.transpose((2, 0, 1))
 
 
 
