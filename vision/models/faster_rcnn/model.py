0973274868a6b1547d1fc42057d6803e388d51b9
Vikram Raj <dr.vikramraj87@live.com>
2020-05-05 20:02:30 +0530

    Model.py created

--- vision/models/faster_rcnn/model.py
+++ vision/models/faster_rcnn/model.py
@@ -1,5 +1,7 @@
-from torch import nn
-from vision.models.faster_rcnn.utils.tests.feature_extractor import decom_vgg16
+from torch import nn, Tensor
+from .feature_extractor import decom_vgg16
+from .region_proposal_network import RegionProposalNetwork
+from .roi_head import ROIHead
 
 
 class Model(nn.Module):
@@ -10,7 +12,33 @@ class Model(nn.Module):
                  n_fg_class=20,
                  ratios=(0.5, 1, 2),
                  scales=(8, 16, 32)):
+        super().__init__()
         extractor, classifier = decom_vgg16()
+
+        self.n_class = n_fg_class + 1
+
+        self.extractor = extractor
+
+        self.rpn = RegionProposalNetwork(512,
+                                         512,
+                                         ratios=ratios,
+                                         scales=scales,
+                                         feat_stride=self.feat_stride)
+
+        self.roi_head = ROIHead(n_class=self.n_class,
+                                roi_size=7,
+                                spatial_scale=1./self.feat_stride,
+                                classifier=classifier)
+
+    def forward(self, x: Tensor, scale=1.):
+        img_size = x.shape[2:]
+
+        feats = self.extractor(x)
+        _, _, rois, roi_ix = self.rpn(feats, img_size, scale)
+        roi_locs, roi_scores = self.roi_head(feats, rois, roi_ix)
+        return roi_locs, roi_scores, rois, roi_ix
+
+
 
 
 
